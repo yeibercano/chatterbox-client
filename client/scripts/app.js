4bc0ce8b2919a63d5ef5c80f646c58f9f39edc46
yeibercano <yeibercano@gmail.com>
2016-03-25 12:32:24 -0700

    working on post method

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,29 @@
-// YOUR CODE HERE:
+var app = {
+  init: function(){},
+  send: function(){
+    $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  type: 'POST',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message sent');
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message');
+  }
+});
+  
+  }
+
+};
+
+var message = {
+  username: 'shawndrost',
+  text: 'trololo',
+  roomname: '4chan'
+};
+
 
